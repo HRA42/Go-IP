a245588f83a14d06b78a29ec41db0ddb9e915118
Henry Rausch <44985923+HRA42@users.noreply.github.com>
2023-08-20 02:32:03 +0200

    Refactor logging in cmd/go-ip/main.go
    Replaced repeated logging with new PrintCIDR() and Print() methods in Network struct to improve code readability, reduce redundancy and increase traceability during debugging. This enhances clarity when displaying network information such as subnet mask, network address, and CIDR notation.

--- cmd/go-ip/main.go
+++ cmd/go-ip/main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"flag"
-	"log"
-
 	"github.com/hra42/Go-IP/internal/network"
 )
 
@@ -14,50 +12,21 @@ func main() {
 		"Gibt die Subnetz Maske zu einer Netzwerk Adresse in CIDR Notation aus.",
 	)
 	flag.Parse()
-	if *cidrFlag {
+
+	switch true {
+	case *cidrFlag:
 		networkAddress := network.InputNetworkAddress()
 		newNetwork := network.Network{
 			IpAddress:  networkAddress.IP,
 			SubnetMask: networkAddress.Mask,
 		}
-		log.Printf("Die Subnetzmaske ist: %d.%d.%d.%d\n",
-			newNetwork.SubnetMask[0],
-			newNetwork.SubnetMask[1],
-			newNetwork.SubnetMask[2],
-			newNetwork.SubnetMask[3],
-		)
-		log.Printf("Die Netzwerk Adresse ist: %v\n",
-			newNetwork.GetNetworkAddress(),
-		)
-		log.Printf("Die erste nutzbare Adresse des Netzwerks ist: %v\n",
-			newNetwork.GetFirstIP(),
-		)
-		log.Printf("Die letzte nutzbare Adresse des Netzwerks ist: %v\n",
-			newNetwork.GetLastIP(),
-		)
-		log.Printf("Die Broadcast Adresse des Netzwerks ist: %v\n",
-			newNetwork.GetBroadcastAddress(),
-		)
-	} else {
+		newNetwork.PrintCIDR()
+	default:
 		newNetwork := network.Network{
 			IpAddress:  network.InputIP(),
 			SubnetMask: network.InputSubnetMask(),
 		}
-		log.Printf("Die CIDR Notation des Netzwerks ist: %v",
-			newNetwork.GetCIDRFromSubnetMask(),
-		)
-		log.Printf("Die Netzwerk Adresse ist: %v\n",
-			newNetwork.GetNetworkAddress(),
-		)
-		log.Printf("Die erste nutzbare Adresse des Netzwerks ist: %v\n",
-			newNetwork.GetFirstIP(),
-		)
-		log.Printf("Die letzte nutzbare Adresse des Netzwerks ist: %v\n",
-			newNetwork.GetLastIP(),
-		)
-		log.Printf("Die Broadcast Adresse des Netzwerks ist: %v\n",
-			newNetwork.GetBroadcastAddress(),
-		)
+		newNetwork.Print()
 	}
 
 }
